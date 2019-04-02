87ca639b20622dcdbb0c036e5609655dfc305547
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-04-02 11:25:45 +0200

    Boletin 26

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,15 @@
+
+package boletin26;
+
+/**
+ *
+ * @author rafsniper99
+ */
+public class Boletin26 {
+
+    public static void main(String[] args) {
+        Ventana Ven1=new Ventana();
+        Ven1.setVisible(true);
+        Ven1.setDefaultCloseOperation(3);
+    }
+}
